8af747dfa2109e2349d59c0e087ec479338eb8db
Extoren <62182617+Extoren@users.noreply.github.com>
2023-04-30 00:41:22 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,192 @@
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// oppdater kameraets sideforhold når størrelsen på vinduet endres
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+let objLoader = new THREE.OBJLoader();
+let houseMesh;
+
+objLoader.load(
+  'Portfolio 2.0 English.obj',
+  function (object) {
+    object.scale.set(0.05, 0.05, 0.05);
+    scene.add(object);
+    houseMesh = object.children[0]; // assuming the house is the first child object
+  }
+);
+
+//bare for lys
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+var pointLight = new THREE.PointLight(0xffffff, 0.2);
+pointLight.position.set(0, 0, 100);
+scene.add(pointLight);
+
+
+camera.position.z = 0; //How close
+camera.position.y = 1; //Up and down
+camera.position.x = 0; //right or left
+
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let cameraX = 0;
+let cameraZ = 0;
+
+function onKeyDown(event) {
+  switch (event.keyCode) {
+    case 87: // W
+      moveForward = true;
+      break;
+    case 83: // S
+      moveBackward = true;
+      break;
+    case 68: // A
+      moveLeft = true;
+      camera.position.x -= 1; // flytt kameraet til venstre langs x-aksen
+      break;
+    case 65: // D
+      moveRight = true;
+      camera.position.x += 1; // flytt kameraet til høyre langs x-aksen
+      break;
+  }
+}
+
+function onKeyUp(event) {
+  switch (event.keyCode) {
+    case 87: // W
+      moveForward = false;
+      break;
+    case 83: // S
+      moveBackward = false;
+      break;
+    case 68: // A
+      moveLeft = false;
+      break;
+    case 65: // D
+      moveRight = false;
+      break;
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  const cameraDirection = new THREE.Vector3();
+  camera.getWorldDirection(cameraDirection);
+
+  //beveg høyre og venstre basert på kamera retning
+  if (moveLeft || moveRight) {
+    const directionFactor = cameraDirection.z >= 0 ? 1 : -1;
+    cameraX += (moveLeft ? -0.05 : 0.05) * directionFactor;
+  }
+
+  //beveg fram og tilbake basert på kamera retning
+  if (moveForward || moveBackward) {
+    const directionFactor = cameraDirection.x >= 0 ? 1 : -1;
+    cameraX += (moveForward ? 0.05 : -0.05) * directionFactor;
+  }
+
+  camera.position.x = cameraX;
+  camera.position.z = cameraZ;
+
+  renderer.render(scene, camera);
+}
+
+
+
+
+document.addEventListener('keydown', onKeyDown, false);
+document.addEventListener('keyup', onKeyUp, false);
+
+animate();
+
+
+
+
+
+
+// mus locker
+function lockPointer() {
+  var pointerLockElement = document.getElementById('pointer-lock');
+
+  pointerLockElement.addEventListener('click', function() {
+    pointerLockElement.requestPointerLock = pointerLockElement.requestPointerLock || pointerLockElement.mozRequestPointerLock || pointerLockElement.webkitRequestPointerLock;
+    pointerLockElement.requestPointerLock();
+  }, false);
+
+  document.addEventListener('pointerlockchange', onPointerLockChange, false);
+  document.addEventListener('mozpointerlockchange', onPointerLockChange, false);
+  document.addEventListener('webkitpointerlockchange', onPointerLockChange, false);
+
+  function onPointerLockChange() {
+    if (document.pointerLockElement === pointerLockElement || document.mozPointerLockElement === pointerLockElement || document.webkitPointerLockElement === pointerLockElement) {
+      document.addEventListener('mousemove', onMouseMove, false);
+      mouseLocked = true;
+    } else {
+      document.removeEventListener('mousemove', onMouseMove, false);
+      mouseLocked = false;
+    }
+  }
+
+  var cameraOrientation = new THREE.Vector3(0, 0, -1);
+var zoomSpeed = 0.1; // Adjust this value to change the speed of the zoom
+
+function onMouseMove(event) {
+  var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+  var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+
+  if (mouseLocked) {
+    // Calculate the new rotation angles and position for the camera based on mouse movements
+    var rotationX = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(1, 0, 0), -movementY * 0.002);
+    var rotationY = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 1, 0), -movementX * 0.002);
+    var rotationZ = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3().crossVectors(cameraOrientation, camera.up), -movementY * 0.002);
+
+    cameraOrientation.applyMatrix4(rotationX);
+    cameraOrientation.applyMatrix4(rotationY);
+    cameraOrientation.applyMatrix4(rotationZ);
+
+    camera.lookAt(camera.position.clone().add(cameraOrientation));
+  }
+}
+
+
+//zoom function
+var cameraOrientation = new THREE.Vector3(0, 0, -1);
+var zoomSpeed = 0.1;
+var maxZoom = 2; // The maximum allowed zoom level
+var minZoom = 1; // The minimum allowed zoom level
+
+function onMouseWheel(event) {
+  var zoomDelta = event.deltaY > 0 ? -1 : 1;
+  var zoomAmount = zoomDelta * zoomSpeed;
+
+  // Check if the new zoom level is within the allowed range
+  var newZoom = camera.zoom + zoomAmount;
+  if (newZoom > maxZoom || newZoom < minZoom) {
+    return; // Do not update the zoom level if it's outside the allowed range
+  }
+
+  // Update the camera's zoom level and projection matrix
+  camera.zoom = newZoom;
+  camera.updateProjectionMatrix();
+}
+
+document.addEventListener('mousemove', onMouseMove, false);
+document.addEventListener('wheel', onMouseWheel, false);
+
+}
+lockPointer ();
+
+
